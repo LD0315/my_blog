f0940b109f66db23cd9adb6abff5f0adc221bf29
linlin <denglinlin315@gmail.com>
2023-01-01 18:10:33 +1000

    update

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Single = () => {
     return (
-        <div className='single'>Single</div>
+        <div className='single'>
+            <div className='content'>
+                <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+            <div className='user'>
+                <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+            <div className='info'>
+                <span>Linlin</span>
+                <p>Posted 2 days ago</p>
+            </div>
+            </div>
+            </div>
+            <div className='menu'>m</div>
+        </div>
     )
 }
 
