cd78a3d37adfe2a83e82d275e1dfdf2ca03609d5
Linlin Deng <linlin.deng@icemedia.com.au>
2023-02-02 09:47:30 +1000

    update2

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <img src={Logo} alt="" />
             <span>
-                Cppyright © 2023 ♥️ made with <b>React.js</b>.
+                Cppyright © 2023 ♥️ made with <b>React.js and Node.js</b>.
             </span>
         </footer>
     )
